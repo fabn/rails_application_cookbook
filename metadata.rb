d14cb5c86aa4a06f952714035102289cd8658e08
Fabio Napoleoni <f.napoleoni@gmail.com>
2013-01-02 15:10:53 +0100

    Improved readme

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,5 @@ description      "Installs/Configures one or more rails application using a stan
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
+recipe "rails_application", "This recipe prepare the target machine to host any number of rails applications"
+
