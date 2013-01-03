90fa55ebd36663f065531f09ce1a18098b21f829
Fabio Napoleoni <f.napoleoni@gmail.com>
2013-01-03 12:27:07 +0100

    Recipe refactoring with custom code

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -10,16 +10,18 @@ include_recipe "database::mysql"
 
 # Fetch applications bag name
 bag_name = node[:rails_application][:applications_bag]
+# All rails applications defined
+rails_applications = data_bag_search(bag_name)
 
 # Return admin credentials that should be used from this databag item
 def admin_credentials data_bag_item
-  # data from databag
+  # mysql credentials data from databag
+  credentials_from_databag = data_bag_item['mysql']['admin_credentials'] if data_bag_item['mysql']
+  credentials_from_databag ||= {} # empty by default if not given through databag
   # uses symbols instead of strings for keys
-  given = mysql_data['admin_credentials'].inject({}) do |h, el|
+  given = credentials_from_databag.inject({}) do |h, el|
     h.update(el.first.to_sym => el.last)
-  end if data_bag_item['admin_credentials']
-  # default value
-  given ||= {}
+  end
   # use defaults where data not provided
   {
       :host => "localhost", :username => 'root',
@@ -27,35 +29,19 @@ def admin_credentials data_bag_item
   }.merge(given)
 end
 
-# Don't install unless at least an app connect to localhost
-install_mysql_server = false
-# see if mysql should be installed on localhost
-data_bag(bag_name).each do |app|
-  # fetch data from item
-  app_data = data_bag_item(bag_name, app)
-  # if app define mysql check if server installation is required
-  if app_data['mysql']
-    # install mysql server only if at least an item uses localhost (or its variants)
-    install_mysql_server ||= [node['fqdn'], 'localhost', '127.0.0.1'].include?(admin_credentials(app_data)[:host])
-  end
-end
-# install the database server if required
-if install_mysql_server
+# If there is an app which uses localhost (or equivalent) as database
+if rails_applications.any? { |app| app['mysql'] && [node['fqdn'], 'localhost', '127.0.0.1'].include?(admin_credentials(app)[:host]) }
   # configure it through node attributes
   include_recipe "mysql::server"
   include_recipe "mysql_charset"
 end
 
 # create application database and users from databag items
-data_bag(bag_name).each do |app|
-  # fetch data from item
-  app_data = data_bag_item(bag_name, app)
-  # mysql data
-  mysql_data = app_data['mysql']
+rails_applications.each do |app|
   # if app define mysql data create database and user with privileges
-  if mysql_data
+  if (mysql_data = app['mysql'])
     # retrieve admin credentials from databag item
-    admin_credentials = admin_credentials(app_data)
+    admin_credentials = admin_credentials(app)
     # create database using admin credentials
     mysql_database mysql_data['database'] do
       connection admin_credentials
