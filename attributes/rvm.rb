3a0a969e086ba358332d8101b5a60bd3a7347049
Fabio Napoleoni <f.napoleoni@gmail.com>
2013-05-23 22:48:45 +0200

    Changed node.set to default (to review)

--- attributes/rvm.rb
+++ attributes/rvm.rb
@@ -7,30 +7,30 @@
 default[:rails_application][:use_rvm] = false
 default[:rails_application][:rvm_install_type] = 'user'
 # Stick with some known defaults for rvm parameters, you can override within the node
-node.set[:rvm][:version] = '1.17.5'
+default[:rvm][:version] = '1.17.5'
 # use last released MRI 1.9.3 version
-node.set[:rvm][:user_default_ruby] = '1.9.3'
-node.set[:rvm][:default_ruby] = '1.9.3'
-node.set[:rvm][:rvmrc] = {
+default[:rvm][:user_default_ruby] = '1.9.3'
+default[:rvm][:default_ruby] = '1.9.3'
+default[:rvm][:rvmrc] = {
     :rvm_project_rvmrc => 0, # cron jobs may not work with this turned on
     :rvm_gemset_create_on_use_flag => 1,
     :rvm_trust_rvmrcs_flag => 1
 }
 # install options for RVM
-node.set[:rvm][:user_installs] = [
+default[:rvm][:user_installs] = [
     {:user => default[:rails_application][:user],
      :home => default[:rails_application][:home],
      :default_ruby => '1.9.3'
     }
 ]
 # global gems for system wide installation
-node.set[:rvm][:global_gems] = [
+default[:rvm][:global_gems] = [
     {:name => 'bundler'},
     {:name => 'rake'},
     {:name => 'rubygems-bundler', :action => 'remove'}
 ]
 # global gems for user installation
-node.set['rvm']['user_global_gems'] = [
+default['rvm']['user_global_gems'] = [
     {:name => 'bundler'},
     {:name => 'rake'},
     {:name => 'rubygems-bundler', :action => 'remove'}
