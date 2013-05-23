41ab7aafac6796a0268df8dd9ee82a429bd92a3e
Fabio Napoleoni <f.napoleoni@gmail.com>
2013-05-23 22:49:10 +0200

    Temporarily disabled rvm attributes

--- attributes/rvm.rb
+++ attributes/rvm.rb
@@ -6,32 +6,30 @@
 # RVM installation is optional and controlled by these params
 default[:rails_application][:use_rvm] = false
 default[:rails_application][:rvm_install_type] = 'user'
-# Stick with some known defaults for rvm parameters, you can override within the node
-default[:rvm][:version] = '1.17.5'
 # use last released MRI 1.9.3 version
-default[:rvm][:user_default_ruby] = '1.9.3'
-default[:rvm][:default_ruby] = '1.9.3'
-default[:rvm][:rvmrc] = {
-    :rvm_project_rvmrc => 0, # cron jobs may not work with this turned on
-    :rvm_gemset_create_on_use_flag => 1,
-    :rvm_trust_rvmrcs_flag => 1
-}
-# install options for RVM
-default[:rvm][:user_installs] = [
-    {:user => default[:rails_application][:user],
-     :home => default[:rails_application][:home],
-     :default_ruby => '1.9.3'
-    }
-]
-# global gems for system wide installation
-default[:rvm][:global_gems] = [
-    {:name => 'bundler'},
-    {:name => 'rake'},
-    {:name => 'rubygems-bundler', :action => 'remove'}
-]
-# global gems for user installation
-default['rvm']['user_global_gems'] = [
-    {:name => 'bundler'},
-    {:name => 'rake'},
-    {:name => 'rubygems-bundler', :action => 'remove'}
-]
+#default[:rvm][:user_default_ruby] = '1.9.3'
+#default[:rvm][:default_ruby] = '1.9.3'
+#default[:rvm][:rvmrc] = {
+#    :rvm_project_rvmrc => 0, # cron jobs may not work with this turned on
+#    :rvm_gemset_create_on_use_flag => 1,
+#    :rvm_trust_rvmrcs_flag => 1
+#}
+## install options for RVM
+#default[:rvm][:user_installs] = [
+#    {:user => default[:rails_application][:user],
+#     :home => default[:rails_application][:home],
+#     :default_ruby => '1.9.3'
+#    }
+#]
+## global gems for system wide installation
+#default[:rvm][:global_gems] = [
+#    {:name => 'bundler'},
+#    {:name => 'rake'},
+#    {:name => 'rubygems-bundler', :action => 'remove'}
+#]
+## global gems for user installation
+#default['rvm']['user_global_gems'] = [
+#    {:name => 'bundler'},
+#    {:name => 'rake'},
+#    {:name => 'rubygems-bundler', :action => 'remove'}
+#]
